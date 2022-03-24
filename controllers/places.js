1837f61a8286882ae62e5ba48baa09511832f3d1
Rocco Mannino <RWMannio@gmail.com>
2022-03-24 19:34:23 -0400

    Assignment Code Done Still Needs Test

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,26 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'https://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'https://placekitten.com/250/250'
+    }, {
+        name: 'Placeholder',
+        city: 'Placeholder',
+        state: 'Placeholder',
+        cuisines: 'Placeholder',
+        pic: 'https://placekitten.com/250/250'
+    }]
+    res.render('places/index', {places})
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', function (req, res) {
-	res.send('Hello World')	
+	res.render('home')	
 })
 
 app.get('*', (req, res) =>{
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render('error404')
 })
 
 app.listen(process.env.PORT)
