1c1130316fcf0fbe4fe0040213682dc0f6a5882a
Rocco Mannino <RWMannio@gmail.com>
2022-04-09 08:37:30 -0400

    Part 9/10 done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,24 +2,54 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">No comments yet.</h3>
+    )
+    
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+    }
+  
     return (
         <Def>
-          <main>
-            <h1>{data.place.name}</h1>
-            <div>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-            </div>
-            <div>
-                <h2>Description</h2>
-                <p>No Description</p>
-            </div>
-            <div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">Delete</button>
-                </form>
-            </div>
+            <main>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <h2>Comments</h2>
+                        {comments}
+                    </div>
+                    <hr/>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                    </form>
+                </div>
           </main>
         </Def>
     )
